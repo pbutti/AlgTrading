0d741de927333bfb2243a051f085b492bec584cf
pbutti <pierfrancesco.butti@gmail.com>
2020-11-20 16:33:53 -0800

    Add computation of the MACD

--- Algs/Alg_MACD.py
+++ Algs/Alg_MACD.py
@@ -7,7 +7,8 @@ import mplfinance as fplt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-def makeCSPlot(df) -> go.Figure:
+# This returns the candlestic and the volume bar.
+def makeCSPlot(df) -> [go.Candlestick,go.Bar]:
 
     # Get the time series in candlestick
     candlestick = go.Candlestick(x=df.index,
@@ -18,28 +19,42 @@ def makeCSPlot(df) -> go.Figure:
     # Get the volumes as bar chart
     volumes = go.Bar(x=df.index, y=df['Volume'],opacity=0.2)
 
-    # Create figure with secondary y-axis
-    fig = make_subplots(specs=[[{"secondary_y":True}]])
-
-    # Add the candle stick and the volumes to the figure
-    fig.add_trace(candlestick,secondary_y=True)
-    fig.add_trace(volumes,secondary_y=False)
-
-    fig.layout.yaxis2.showgrid=False
-
-    return fig
+    return [candlestick,volumes]
 
 def main():
     print("MACD Alg")
     #Get some data
-    data: pandas.DataFrame = yfinance.download('AAPL', '2020-1-1', '2020-11-18')
+    data = yfinance.download('AAPL', '2019-11-19', '2020-11-20')
+
+    # Form the candlestick and volume plots
+    candlestick,volumes = makeCSPlot(data)
+
     print(data.columns)
-    print(data.index)
-    print(data.head())
-    fig = plt.figure()
-    data['Adj Close'].plot()
-    plotly_fig = makeCSPlot(data)
-    plotly_fig.show()
+    # Clean nan values
+    df = ta.utils.dropna(data)
+
+    print(type(data['Close']))
+    #Compute the MACD
+    # macd = ta.add_trend_ta(data,'High','Low','Close',True) #The ADX indicator is broken?
+
+    MACD_obj = ta.trend.MACD(data['Close'])
+
+    # Create the macd trace for plotting
+    macd_scatter = go.Scatter(x=data.index,y=MACD_obj.macd())
+
+    # Create figure with secondary y-axis
+    fig = make_subplots(rows=2,cols=1,specs=[[{"secondary_y": True}],[{"secondary_y" : False}]])
+
+    # Add the candle stick and the volumes to the figure
+    fig.add_trace(candlestick,row=1,col=1, secondary_y=True)
+    fig.add_trace(volumes,row=1,col=1, secondary_y=False)
+    fig.layout.yaxis2.showgrid = False
+
+
+    fig.add_trace(macd_scatter,row=2,col=1)
+    fig.show()
+
+    #plotly_fig.show()
 
 if __name__ == "__main__":
     main()
