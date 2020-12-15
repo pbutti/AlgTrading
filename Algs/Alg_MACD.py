31919037bb73d0c7c4c1d205a539363872dbf666
pbutti <pierfrancesco.butti@gmail.com>
2020-12-15 14:55:24 -0800

    Update Alg_MACD.py

--- Algs/Alg_MACD.py
+++ Algs/Alg_MACD.py
@@ -94,18 +94,36 @@ def testtrades_2(df,intersections, insights,pat=0,nsec=1,tax=0.2):
     profit = 0
     buy_amount = 0
     sell_amount = 0
+    offset = 0
 
-    for i in range(len(intersections) - pat):
-        index=intersections[i]+pat
+    # Check if the last action was buying a security. If so then only compute up to n-1 actions.
+    # Last profit will be computed differently.
+    if (insights[-1] == "buy"):
+        offset=1
+
+    for i in range(len(intersections) - offset):
+        index=intersections[i] + pat
         action_close_price = df['Close'][index]
+        print("Action price:", df['Close'][index], insights[i])
         if insights[i] == "buy":
             buy_amount+=action_close_price
         else:
             sell_amount+=action_close_price
 
-    profit = nsec*(sell_amount - buy_amount) * (1-tax)
+    profit = nsec*(sell_amount - buy_amount)
+    if profit > 0:
+        profit *= (1-tax)
 
-    return profit
+    # Compute the profit for the last buy we made: check the price we bought with the current price.
+    # Since that are unrealized gains we don't have to tax them.
+
+    last_buy_close_price = df['Close'][intersections[-1]]
+    current_price = df['Close'][-1]
+    unrealized_profit = nsec*(current_price - last_buy_close_price)
+
+    print(last_buy_close_price,current_price,unrealized_profit)
+
+    return profit + unrealized_profit
 
 def makefullplot(df,MACD_obj, intersections=[],insights=[]):
 
@@ -166,12 +184,12 @@ def makefullplot(df,MACD_obj, intersections=[],insights=[]):
 def main():
     print("MACD Alg")
     #Config:
-    ticker="AAPL"
+    ticker="BTC-USD"
     StartDate = '2020-08-23'
     #Get some data
     today = date.today().strftime("%Y-%m-%d")
     #data: Union[Union[DataFrame, Series], Any] = yfinance.download(ticker, StartDate, today,interval='1h')
-    data: Union[Union[DataFrame, Series], Any] = yfinance.download(ticker, period='3mo', interval='1d')
+    data: Union[Union[DataFrame, Series], Any] = yfinance.download(ticker, period='6mo', interval='1d')
     print(data.head)
     print(data.columns)
 
@@ -183,21 +201,20 @@ def main():
     #trends = ta.add_trend_ta(data,'High','Low','Close',True) #The ADX indicator is broken?
 
     #For longer trading slow = 26, fast = 12, signal = 9
-    #For weekly trading skiw = 35, fast = 5 , signal = 5
+    #For weekly trading slow = 35, fast = 5 , signal = 5
     #MACD_obj = ta.trend.MACD(data['Close'],n_slow=35,n_fast=5,n_sign=5)
     #MACD_obj = ta.trend.MACD(data['Close'], n_slow=26, n_fast=12, n_sign=9)
     MACD_obj = ta.trend.MACD(data['Close'])
     print(MACD_obj)
-
 
     intersections,insights = getMACD_intersections(MACD_obj.macd_signal(), MACD_obj.macd())
     print(intersections)
     print(insights)
 
 
-    profit = TestTrades(data,intersections,insights,1,1)
+    profit = TestTrades(data,intersections,insights,0,1)
     print("profit=",profit)
-    profit_2 = testtrades_2(data,intersections,insights,0,1,tax=0.)
+    profit_2 = testtrades_2(data,intersections,insights,0,0.1,tax=0.35)
     print("profit_2",profit_2)
 
     makefullplot(data,MACD_obj,intersections,insights)
